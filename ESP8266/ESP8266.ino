ffa4503b6e22dc3a684c89a98982c5a987e26dfc
Tauki Tahmid <ttauki@gmail.com>
2018-11-21 18:39:39 +0600

    Connect ESP8266 with the WiFi router

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -1,13 +1,18 @@
+#include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
 
 #define LED0 2 // the one at the board
-#define baud 9600 
+#define baud 9600
+#define ssid "---------"
+#define ssid_password "---------"
+#define DEBUG true
+#define DEBUGBaud 115200
 
 SoftwareSerial main(12, 13); // RX, TX
 
 void setup() {
   
-  pinMode(LED, OUTPUT);
+  pinMode(LED0, OUTPUT);
 
   // begin main at baud rate: 9600
   main.begin(baud);
@@ -27,6 +32,21 @@ void setup() {
 
   // blink 3 times upon successful communication with the main board.
   Blink(LED0, 3, 200);
+  
+  // connect WiFi
+  WiFi.begin(ssid, ssid_password);
+  
+  // wait while WiFi connects
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  // blink thrice upon connection
+  Blink(LED0, 3, 500);
+
+  if(DEBUG) {
+    debugWiFi();
+  }
 }
 
 
@@ -42,4 +62,23 @@ void Blink(int pin, int times, int DELAY){
     digitalWrite(pin, LOW);
     delay(DELAY);
   }
+}
+
+void debugWiFi() {
+  Serial.begin(DEBUGBaud);
+  delay(100);
+ 
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Netmask: ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: ");
+  Serial.println(WiFi.gatewayIP());
 }
