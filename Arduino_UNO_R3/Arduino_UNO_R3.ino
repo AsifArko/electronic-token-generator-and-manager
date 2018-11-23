bcb473ea80d2fe92b37cbce41cc222aad0491f9f
Tauki Tahmid <ttauki@gmail.com>
2018-11-24 02:33:23 +0600

    - Interface SIM800L with the arduino - Refactor

--- Arduino_UNO_R3/Arduino_UNO_R3.ino
+++ Arduino_UNO_R3/Arduino_UNO_R3.ino
@@ -1,23 +1,50 @@
 #include <SoftwareSerial.h>
+#include <Sim800L.h>
 
-#define baud 9600
+// baud rates
+#define baud_ESP 9600
+#define baud_GSM 4800
+
 #define DEBUG true
 #define DEBUGBaud 115200
+
 #define ACK "ACK"
 
-SoftwareSerial ESP8266E(7, 8); // RX, TX
+// RX, TX
+SoftwareSerial ESP8266E(7, 8); 
+Sim800L GSM(11, 10);
 
 void setup() {
   // set BULTIN LED as an output
   pinMode(LED_BUILTIN, OUTPUT);
   
   // begin ESP8266
-  ESP8266E.begin(baud);
+  ESP8266E.begin(baud_ESP);
 
   // wait while it connects
   while(!ESP8266E) {
     ;
   }
+
+  // blink thrice
+  Blink(LED_BUILTIN, 3, 500);
+
+  // blink once for 1 sec
+  Blink(LED_BUILTIN, 1, 1000);
+
+  // begin SIM800L
+  GSM.begin(baud_GSM);
+
+  // wait while it connects
+  while(!GSM){
+    ;
+  }
+
+  // blink thrice
+  Blink(LED_BUILTIN, 3, 300);
+
+  // blink once for 1 sec
+  Blink(LED_BUILTIN, 1, 1000);
   
 }
 
